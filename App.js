a1354e191342d4c082b7615020d794fb158a11fc
Jay Hernandez <jeric614@yahoo.com>
2022-01-14 10:19:24 +0900

    initial commit f/food app

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { CLIENT, TOKEN } from '@env';
 
 export default function App() {
   return (
