329d9dfc930686609283f07c8577ded835c1514d
Jay Hernandez <jeric614@yahoo.com>
2022-01-17 15:47:41 +0900

    rendered results w/flatlist

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,11 +1,18 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar';
 import useResults from '../hooks/useResults';
+import ResultsList from '../components/ResultsList';
 
 const SearchScreen = () => {
   const [term, setTerm] = useState('');
   const [searchApi, results, errorMessage] = useResults();
+
+  const filterResultsByPrice = (price) => {
+    return results.filter((business) => business.price === price);
+  };
+
+  filterResultsByPrice();
 
   return (
     <View>
@@ -16,6 +23,9 @@ const SearchScreen = () => {
       />
       {errorMessage ? <Text>{errorMessage}</Text> : null}
       <Text>We have found {results.length} results</Text>
+      <ResultsList results={filterResultsByPrice('$')} title="Cost Effective" />
+      <ResultsList results={filterResultsByPrice('$$')} title="Bit Pricier" />
+      <ResultsList results={filterResultsByPrice('$$$')} title="Big Spender" />
     </View>
   );
 };
@@ -23,3 +33,40 @@ const SearchScreen = () => {
 const styles = StyleSheet.create({});
 
 export default SearchScreen;
+
+const YELP = [
+  {
+    rating: 4,
+    price: '$',
+    phone: '+14152520800',
+    id: 'E8RJkjfdcwgtyoPMjQ_Olg',
+    alias: 'four-barrel-coffee-san-francisco',
+    is_closed: false,
+    categories: [
+      {
+        alias: 'coffee',
+        title: 'Coffee & Tea',
+      },
+    ],
+    review_count: 1738,
+    name: 'Four Barrel Coffee',
+    url: 'https://www.yelp.com/biz/four-barrel-coffee-san-francisco',
+    coordinates: {
+      latitude: 37.7670169511878,
+      longitude: -122.42184275,
+    },
+    image_url:
+      'http://s3-media2.fl.yelpcdn.com/bphoto/MmgtASP3l_t4tPCL1iAsCg/o.jpg',
+    location: {
+      city: 'San Francisco',
+      country: 'US',
+      address2: '',
+      address3: '',
+      state: 'CA',
+      address1: '375 Valencia St',
+      zip_code: '94103',
+    },
+    distance: 1604.23,
+    transactions: ['pickup', 'delivery'],
+  },
+];
