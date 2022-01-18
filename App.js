e2936f8c3991d657ef0a544e5a262a1eee006466
Jay Hernandez <jeric614@yahoo.com>
2022-01-18 10:05:18 +0900

    navigate from child component

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import SearchScreen from './src/screens/SearchScreen';
+import ResultsShowScreen from './src/screens/ResultsShowScreen';
 
 const { Navigator, Screen } = createNativeStackNavigator();
 
@@ -18,6 +19,7 @@ const App = () => {
         }}
       >
         <Screen name="Search" component={SearchScreen} />
+        <Screen name="ResultsShow" component={ResultsShowScreen} />
       </Navigator>
     </NavigationContainer>
   );
